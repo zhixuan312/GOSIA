edf59f61a3e22048c6f3a55266c9014836dcdda7
xuan <zhangzhixuan312@gmail.com>
2017-05-28 20:13:55 +0800

    test commit 10

--- app/src/main/java/csl/sia/com/gosia/LoginActivity.java
+++ app/src/main/java/csl/sia/com/gosia/LoginActivity.java
@@ -135,9 +135,6 @@ public class LoginActivity extends AppCompatActivity {
             cancel = true;
         }
 
-
-
-
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
             login_edit_email.setError(getString(R.string.error_field_required));
